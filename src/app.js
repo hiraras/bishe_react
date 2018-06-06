262e67fef3940ed416ccdbff110bd1c3accfc8be
MS-20170903V9\Administrator <kazilagui@163.com>
2018-06-06 19:47:10 +0800

    创建项目

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component{
+    render(){
+        return (
+            <div>
+                123
+            </div>
+        )
+    }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
