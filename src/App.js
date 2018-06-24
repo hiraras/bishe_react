49a84bcddeb6850a337d0414c5134ca4333a2b80
hiraras <kazilagui@163.com>
2018-06-24 11:59:02 +0800

    使用eject命令使项目能够被用户配置并添加css-modules

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styles from './App.css';
+
+class App extends Component{
+    render() {
+        return (
+            <div className={styles.test}>
+                111
+            </div>
+        );
+    }
+}
+
+export default App;
+
